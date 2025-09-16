3700a2d41354d132f32ada2dcc91ed07c2a32a45
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-09-16 12:34:14 -0400

    use cert

--- infrastructure/lib/cloudfront-s3-stack.ts
+++ infrastructure/lib/cloudfront-s3-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
 import { Construct } from "constructs";
 import { createTags } from "./utils/tags";
 
@@ -13,6 +14,19 @@ export class CloudFrontS3Stack extends cdk.Stack {
     super(scope, id, props);
 
     const tier = process.env.TIER || "dev";
+    const sslCertificateArn = process.env.SSL_CERTIFICATE_ARN;
+
+    // Define custom domain and certificate if SSL certificate ARN is provided
+    const domainName = `pedigree-${tier}.cancer.gov`;
+    let certificate: certificatemanager.ICertificate | undefined;
+
+    if (sslCertificateArn) {
+      certificate = certificatemanager.Certificate.fromCertificateArn(
+        this,
+        "SSLCertificate",
+        sslCertificateArn
+      );
+    }
 
     // Create S3 bucket for hosting frontend files
     this.bucket = new s3.Bucket(this, "FrontendBucket", {
@@ -28,34 +42,41 @@ export class CloudFrontS3Stack extends cdk.Stack {
       cdk.Tags.of(this.bucket).add(key, value);
     });
 
+    // Create CloudFront distribution configuration
+    const distributionConfig: cloudfront.DistributionProps = {
+      comment: `CF distribution for pedigree-${tier}.cancer.gov`,
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(this.bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        originRequestPolicy: cloudfront.OriginRequestPolicy.CORS_S3_ORIGIN,
+      },
+      defaultRootObject: "index.html",
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: "/index.html",
+        },
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: "/index.html",
+        },
+      ],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100, // Use only North America and Europe
+      // Add custom domain and certificate if SSL certificate ARN is provided
+      ...(certificate && {
+        domainNames: [domainName],
+        certificate: certificate,
+      }),
+    };
+
     // Create CloudFront distribution
     this.distribution = new cloudfront.Distribution(
       this,
       "FrontendDistribution",
-      {
-        comment: `CF distribution for pedigree-${tier}.cancer.gov`,
-        defaultBehavior: {
-          origin: origins.S3BucketOrigin.withOriginAccessControl(this.bucket),
-          viewerProtocolPolicy:
-            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
-          originRequestPolicy: cloudfront.OriginRequestPolicy.CORS_S3_ORIGIN,
-        },
-        defaultRootObject: "index.html",
-        errorResponses: [
-          {
-            httpStatus: 404,
-            responseHttpStatus: 200,
-            responsePagePath: "/index.html",
-          },
-          {
-            httpStatus: 403,
-            responseHttpStatus: 200,
-            responsePagePath: "/index.html",
-          },
-        ],
-        priceClass: cloudfront.PriceClass.PRICE_CLASS_100, // Use only North America and Europe
-      }
+      distributionConfig
     );
 
     // Add tags to CloudFront distribution
@@ -80,5 +101,18 @@ export class CloudFrontS3Stack extends cdk.Stack {
       value: this.distribution.distributionId,
       description: "CloudFront Distribution ID",
     });
+
+    // Output custom domain information if certificate is configured
+    if (certificate) {
+      new cdk.CfnOutput(this, "CustomDomainName", {
+        value: domainName,
+        description: "Custom Domain Name",
+      });
+
+      new cdk.CfnOutput(this, "CustomDomainURL", {
+        value: `https://${domainName}`,
+        description: "Custom Domain URL",
+      });
+    }
   }
 }
